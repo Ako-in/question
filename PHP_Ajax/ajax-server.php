d2fefe3c1faf9752442ee2f143b02880d12f70f5
Ako-in <sinba.805i@gmail.com>
2024-04-16 22:15:38 +0700

    質問用最新版

--- PHP_Ajax/ajax-server.php
+++ PHP_Ajax/ajax-server.php
@@ -1,24 +1,28 @@
 <?php
 //Ajaxリクエストを取得
+// var_dump('111');
 $ajax_request = file_get_contents('php://input');
-
+// var_dump($ajax_request);
 //AjaxリクエストをPHPで扱える連想配列に変換
 $data = json_decode($ajax_request,true);
 
 //受け取ったデータに応じて処理を行う
+// var_dump($data);
 if($data['name']==='SAMURAI'){
-  $data['name']= 'TERAKOYA';
-}else{
-  $data['name'] = 'SAMURAI';
-}
+  $data['name']='TERAKOYA';
+};
 
 //Ajaxレスポンスを生成(連想配列としてセット)
+var_dump('111');
 $response = [
   'message'=>$data['name']
 ];
-
+var_dump('222');
 //JSON形式を指定してブラウザ側へ返信
-header('Content-Type:applicaation/json');
+
+header('Content-Type:application/json');
+var_dump(json_encode($response));
+var_dump('333333');
 echo json_encode($response);
 
 ?>
